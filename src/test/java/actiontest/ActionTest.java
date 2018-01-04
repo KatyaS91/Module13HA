fe9f2c372bd0f80c6fb7b7590da26bc8a2cbbdc3
Skarzhynskaya, Katsiaryna <katsiaryna.skarzhynskaya@kronos.com>
2018-01-04 15:48:50 +0300

    action test

---
+++ src/test/java/actiontest/ActionTest.java
@@ -0,0 +1,27 @@
+package actiontest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+
+import static utils.JsUtils.highlightElement;
+
+/**
+ * Created by Katsiaryna_Skarzhyns on 1/4/2018.
+ */
+public class ActionTest extends BaseTestPage {
+
+	@Test(description = "Action checking", groups = "p1")
+	public void create_mail() throws InterruptedException {
+		LoginPage loginPage = new LoginPage(driver);
+		highlightElement(driver, loginPage.loginInput);
+		doubleClick(loginPage.moreOptionsBtn);
+		Assert.assertTrue(loginPage.isMenuDisplayed(), "Menu popup isn't displayed");
+		hover(loginPage.nextBtn);
+		Assert.assertTrue(loginPage.isCursorPointer(), "Cursor isn't change to pointer");
+		makeScreenshots();
+		loginPage.loginInput.sendKeys("test");
+		clearInputViaHotKeys(loginPage.loginInput);
+		Assert.assertTrue(loginPage.isInputEmpty(loginPage.loginInput), "Login input isn't empty");
+	}
+}
